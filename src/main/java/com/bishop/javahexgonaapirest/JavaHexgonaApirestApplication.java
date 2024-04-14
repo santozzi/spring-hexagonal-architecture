a0f871395b245a04d4e182d34fb29605bbba0c60
Sergio Antozzi <santozzi@gmail.com>
2024-04-14 17:22:49 -0300

    architecture package

---
+++ src/main/java/com/bishop/javahexgonaapirest/JavaHexgonaApirestApplication.java
@@ -0,0 +1,13 @@
+package com.bishop.javahexgonaapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHexgonaApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHexgonaApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bishop/javahexgonaapirest/JavaHexgonaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishop.javahexgonaapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHexgonaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
